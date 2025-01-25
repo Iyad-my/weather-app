4b37de82e36383dd8983b8dceb0efb3d32ec6109
iyad <imoucharrafie@gmail.com>
2025-01-25 17:14:13 -0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+ import GetLocation from './components/GetLocation.vue';
 
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  
+  <GetLocation />
 </template>
 
 <style scoped>
